a5e8dd31559e4d5d3216c8e969e17b0366160791
Damian <zinlinhtun34@gmail.com>
2025-02-01 04:37:29 +0000

    added urdf

--- adsmt_description/launch/display.launch.py
+++ adsmt_description/launch/display.launch.py
@@ -2,30 +2,43 @@ import os
 import launch
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, Command
 
 def generate_launch_description():
-    urdf_file = os.path.join(get_package_share_directory('adsmt_description'), 'simulation', 'adsmt.urdf')
+    pkg = get_package_share_directory('adsmt_description')
+    urdf_file = os.path.join(pkg, 'urdf', 'adsmt.urdf')
+    config = os.path.join(pkg, 'rviz', 'config.rviz')
 
+    # Declare the robot_state_publisher node
+    robot_pub_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': open(urdf_file).read()
+        }]
+    )
+
+    # Declare the joint_state_publisher_gui node
+    joint_pub_node = launch_ros.actions.Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # Declare the rviz2 node
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+    )
+
+    # Return the launch description
     return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'robot_description': open(urdf_file).read(),
-                'initial_pose': {'x': 0.0, 'y': 0.0, 'z': 0.0, 'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0}
-            }]
-        ),
-        launch_ros.actions.Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui',
-            output='screen'
-        ),
-        launch_ros.actions.Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen'
-        )
+
+        robot_pub_node,
+        joint_pub_node,
+        rviz_node
     ])
