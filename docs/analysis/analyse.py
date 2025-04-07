841308357c372ec130d9aaf135ac0192655df28f
Damian <zinlinhtun34@gmail.com>
2025-04-07 06:06:52 +0100

    finalisation

--- docs/analysis/analyse.py
+++ docs/analysis/analyse.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load CSV file
-file_path = 'docs/analysis/test_data_logs/bridge/odo-bridge.csv'
+file_path = 'docs/analysis/test_data_logs/kinematics/odo_3_1.csv'
 df = pd.read_csv(file_path)
 
 # Plot ros y vs ros x, x = ros y, y = ros x
 plt.figure(figsize=(10, 6))
-plt.plot(df['y'], df['x'], marker='o', linestyle='-', color='orange',label='ros-x vs ros-y')
+plt.plot(df['y'], df['x'], marker='.', linestyle='-', color='orange',label='ros-x vs ros-y')
 plt.title('Plot')
 plt.xlabel('ros y')
 plt.ylabel('ros x')
@@ -16,9 +16,13 @@ plt.legend()
 plt.grid(True)
 plt.axis('equal')
 
+# Add horizontal line at ros x location of need
+plt.axhline(y=-0.3, color='red', linestyle='--', linewidth=1.5, label='x = -0.5')
+plt.axvline(x=0.1, color='red', linestyle='--', linewidth=1.5, label='x = 0.1')
+
 # Set axis limits to cover the full range of data
 plt.xlim(df['y'].min(), df['y'].max())
-plt.ylim(df['x'].min(), df['x'].max())
+plt.ylim(df['x'].max(), df['x'].min())
 print(df['x'].min(), df['x'].max())
 print(df['y'].min(), df['y'].max())
 
---
+++ docs/meetings_proof/march27_meet.md
@@ -0,0 +1,11 @@
+#  Project Finalisation
+
+- added Results
+- added Discussion
+
+# New results
+- software arhitecture testing results
+- prototype design testing - `Gazebo`
+- prototype design testing - `Real-world`
+- bridging gaps in `simulated` and `real-world` test
+
